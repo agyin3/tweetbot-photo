6ec39ed81d0581ea0430a894b1dfbb35e1157c9a
Buddy Agyin <agyin3@gmail.com>
2020-01-30 11:26:23 -0700

    commit all messages

---
+++ bot.js
@@ -0,0 +1,50 @@
+const Twit = require('twit');
+const config = require('./config.js');
+const tweetBot = new Twit(config);
+
+const params = {
+    q: '#nature AND #photography filter:media',
+    count: 10,
+    lang: 'en'
+}
+
+const favParams = {
+    q: '#photography OR #nature filter:media',
+    count: 15,
+    lang: 'en'
+}
+
+tweetBot.get('search/tweets', params,  (err, data, res) => {
+    if(!err){
+        for(let i=0; i < data.statuses.length; i++){
+            let tweetID = {id: data.statuses[i].id_str}
+            tweetBot.post('statuses/retweet', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Retweet successful`)
+                }else{
+                    console.log(err)
+                }
+            })
+        }
+    }else{
+        console.log(err)
+    }
+})
+
+tweetBot.get('search/tweets', favParams,  (err, data, res) => {
+    if(!err){
+        for(let i=0; i < data.statuses.length; i++){
+            let tweetID = {id: data.statuses[i].id_str}
+            tweetBot.post('favorites/create', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Favorite successful`)
+                }else{
+                    console.log(err)
+                }
+            })
+        }
+    }else{
+        console.log(err)
+    }
+})
+
